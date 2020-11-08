95d6fcf8c0f5a0cb7a74ef38b809fa09066e35f3
Smit417263 <56094552+Smit417263@users.noreply.github.com>
2020-11-08 13:28:27 -0800

    Update [spate130@cs.ucr.edu]_lab5_part1.c

--- turnin/[spate130@cs.ucr.edu]_lab5_part1.c
+++ turnin/[spate130@cs.ucr.edu]_lab5_part1.c
@@ -6,6 +6,7 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ #	Demo Link: https://youtu.be/i3PE-0ckZ0c
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
